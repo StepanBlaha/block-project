7f7f921c109bcbb7654114d5e65670802b27ac41
stepan blaha <stepa15.b@gmail.com>
2025-01-22 15:52:38 +0100

    made some functions for working with the database

--- src/app/db-functions.js
+++ src/app/db-functions.js
@@ -3,7 +3,7 @@ import clientPromise from "../lib/mongodb.js";
 import { MongoClient } from 'mongodb';
 
 // Your MongoDB connection URI (ensure it is stored securely in an environment variable)
-const uri = process.env.MONGODB_URI; // It's important to use .env to store sensitive info
+const uri = process.env.MONGODB_URI; 
 
 const client = new MongoClient(uri);
 
@@ -20,16 +20,35 @@ async function connectToMongo() {
   }
 }
 
-async function connectToDB(dbName, collectionName) {
+//Async function that connects to MongoDB
+async function connectToDB(dbName) {
+  const uri = process.env.MONGODB_URI; 
+  const client = new MongoClient(uri);
   try {
     await client.connect();
     console.log('Connected to MongoDB!');
     const db = client.db(dbName);
-    const collection = db.collection(collectionName);
+    return db
   } catch (error) {
     console.error('Error connecting to MongoDB', error);
   }
 }
+//Async function that connects to desired collection
+async function connectToCollection(db, collectionName) {
+  const collection = db.collection(collectionName);
+  return collection;
+  
+}
+//Async function that finds all documents in a collection
+async function findDocuments(collection) {
+  const documents = await collection.find().toArray();
+  return documents;
+}
+
+const fdb = await connectToDB('blog');
+const fcollection = await connectToCollection(fdb, 'posts');
+const fdocuments = await findDocuments(fcollection);
+console.log(fdocuments);
 // Call the function to connect
-connectToMongo();
+//connectToMongo();
 
