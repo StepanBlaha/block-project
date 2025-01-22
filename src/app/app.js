7660e9141645a721ee7f373898e29f622aa2fd02
stepan blaha <stepa15.b@gmail.com>
2025-01-22 16:01:58 +0100

    started work on the canvas

---
+++ src/app/app.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react'
+
+function DrawingApp() {
+
+
+    return (
+        <>
+            <h1>Drawing App</h1>
+        </>
+    )
+    
+}
+
+export default DrawingApp
