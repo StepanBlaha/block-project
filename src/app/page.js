d222cf54aff3cb2194bcb0b80a8c88ff9f78ee80
stepan blaha <stepa15.b@gmail.com>
2025-01-23 15:15:02 +0100

    made a brush size reguator

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import "./../styles/canvas.css";
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef, use } from 'react'
 import { document } from "postcss";
 
 function Canvas({moveFunc}) {
@@ -19,6 +19,10 @@ function Canvas({moveFunc}) {
 export default function Home() {
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
   const [isDrawing, setDrawing] = useState(false)
+  const brushSizeRef = useRef(null)
+  const brushSize = useRef(1)
+
+  const brushColor = useRef("#ffffffs")
   const ctxRef = useRef(null)
   const canvasRef = useRef(null)
 
@@ -28,7 +32,7 @@ export default function Home() {
     ctx.strokeStyle = "#ffffff";
     ctx.lineWidth = 10;
     ctxRef.current = ctx
-  })
+  },[])
 
   function mouseDownHandle(){
     setDrawing(true)
@@ -49,22 +53,70 @@ export default function Home() {
   function mouseUpHandle(){
     setDrawing(false)
   }
+  function changeBrushColor() {
+    
+  }
 
+  //Function for changing brush size
+  function changeBrushSize(){
+    brushSize.current = brushRef.current.value
+  }
+  //Detects the change in brush size and updates the canvas brush size settings
+  useEffect(() => { 
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineWidth = brushSize.current
+    ctxRef.current = ctx
+  }, [brushSize.current])
 
 
 
   return (
     <>
-      <h1>Drawing App</h1>
-      <canvas></canvas>
-      <p>X: {mousePos.x}, Y: {mousePos.y}</p>
-      <Canvas moveFunc={mouseMoveHandle}/>
-      <canvas id="myCanvas" width="400" height="200" 
-      ref={canvasRef}
-      onMouseMove={mouseMoveHandle} 
-      onMouseDown={mouseDownHandle}
-      onMouseUp={mouseUpHandle}
-      ></canvas>
+      <div className="Main">
+        <div className="Title">
+          <h1>Drawing App</h1>
+        </div>
+        <div className="Sidebar">
+          <div className="SettingsMenu">
+            <div className="SettingsMenuItem">
+              <div className="SettingsMenuItemTitle">
+                <p>Brush Size</p>
+              </div>
+              <form id="brushSizeForm">
+                <input type="range" name="brushSize" id="brushSize" min="1" max="100" step="1" onChange={changeBrushSize}  ref={brushSizeRef}/>
+              </form>
+            </div>
+
+            <div className="SettingsMenuItem">
+              <div className="SettingsMenuItemTitle">
+                <p>Brush color</p>
+              </div>
+              <form id="brushColorForm">
+                <input type="color" name="brushColor" id="brushColor"  onChange={changeBrushSize}  ref={brushColorRef}/>
+              </form>
+            </div>
+
+          </div>
+        </div>
+        <div className="Content">
+
+          <div className="ContentTitle">
+            <p>X: {mousePos.x}, Y: {mousePos.y}</p>
+          </div>
+
+          <div className="ContentCanvas">
+            <canvas id="myCanvas" width="400" height="200" 
+            ref={canvasRef}
+            onMouseMove={mouseMoveHandle} 
+            onMouseDown={mouseDownHandle}
+            onMouseUp={mouseUpHandle}
+            ></canvas>
+          </div>
+
+        </div>
+
+      </div>
 
     </>
   );
