71a4ef43ac425b30738b1f21daaab096d8c36ce0
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-02-02 15:04:27 +0100

    Default function of the save&export menu

--- src/app/page.js
+++ src/app/page.js
@@ -114,6 +114,9 @@ const Home = () => {
   }
 
 
+
+
+
   //Dictionary that calls the correct function based on the selected tool
   const canvasClickActions = {
     "brush": mouseDownHandle,
@@ -529,9 +532,16 @@ const Home = () => {
 
 
 
+ const actionMenuRef = useRef(null)
 
-
-
+function showSaveActionMenu(){
+  const menu = actionMenuRef.current
+  menu.style.display = "flex"
+}
+function hideSaveActionMenu(){
+  const menu = actionMenuRef.current
+  menu.style.display = "none"
+}
 
 
 
@@ -545,12 +555,18 @@ const Home = () => {
         <div className="Menu">
           <div className="ActionMenu">
             
-            <div className="ActionTool">
+            <div className="ActionTool" id="SaveMenuButton" onMouseOver={showSaveActionMenu} onMouseOut={hideSaveActionMenu}>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-floppy size-10" viewBox="0 0 16 16" onClick={sendData}>
                 <path d="M11 2H9v3h2z"/>
                 <path d="M1.5 0h11.586a1.5 1.5 0 0 1 1.06.44l1.415 1.414A1.5 1.5 0 0 1 16 2.914V14.5a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 14.5v-13A1.5 1.5 0 0 1 1.5 0M1 1.5v13a.5.5 0 0 0 .5.5H2v-4.5A1.5 1.5 0 0 1 3.5 9h9a1.5 1.5 0 0 1 1.5 1.5V15h.5a.5.5 0 0 0 .5-.5V2.914a.5.5 0 0 0-.146-.353l-1.415-1.415A.5.5 0 0 0 13.086 1H13v4.5A1.5 1.5 0 0 1 11.5 7h-7A1.5 1.5 0 0 1 3 5.5V1H1.5a.5.5 0 0 0-.5.5m3 4a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V1H4zM3 15h10v-4.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5z"/>
               </svg>
+              <div className="ActionSaveMenu" ref={actionMenuRef}    >
+                  <div className="ActionSaveItem"></div>
+                  <div className="ActionSaveItem"></div>
+                  <div className="ActionSaveItem"></div>
+              </div>
             </div>
+
             
             <div className="ActionTool">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash size-10" viewBox="0 0 16 16" onClick={clearCanvas}>
