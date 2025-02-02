e60189375b684f5512f666d9d0164c82cb86dc11
Štěpán Bláha <blahast23@sps-prosek.cz>
2025-02-02 23:33:17 +0100

    minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -706,7 +706,7 @@ const Home = () => {
                       </svg>
                     </li>
 
-                    <li className="ShapeSelectItem">
+                    <li className="ShapeSelectItem" id="BottomShape">
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-circle size-8" viewBox="0 0 16 16" onClick={() => setSelectedTool("circle")}>
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                       </svg>
